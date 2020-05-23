3e9eb5237317e8e47ab6bc28bda01c02b3aae3a1
Colton Bonner <cbonner26118@gmail.com>
2020-05-22 21:16:29 -0700

    Connection bug fixes

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 module.exports = {
   migrationDirectory: 'migrations',
   driver: 'pg',
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    process.env.DATABASE_URL === 'test'
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
   ssl: !!process.env.SSL,
 };
--- src/config.js
+++ src/config.js
@@ -4,4 +4,7 @@ module.exports = {
   DATABASE_URL:
     process.env.DATABASE_URL ||
     'postgresql://dunder_mifflin:Tarkov8913@localhost/noteful',
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    'postgresql://dunder_mifflin:Tarkov8913@localhost/noteful-test',
 };
